118dec5ec566e016cb21a3ba1387a4e972d31bec
Kelaven <kevin.lavenant@live.fr>
2024-09-24 14:42:44 +0200

    feat(AddTodo): feature added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,23 @@ import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(content) {
+    const todo = {
+      id: crypto.randomUUID,
+      content,
+      done: false,
+      edit: false,
+    };
+    setTodoList([...todoList, todo]); // récupérer toutes les anciennes todo avec le spread operator + celle que l'on vient de créer
+  }
+
   return (
-    <div className="flex flex-row justify-center items-center p-20">
-      <div className="card p-20">
+    <div className="p-20">
+      <div className="card">
         <h1 className="mb-20">To do list</h1>
-        <AddTodo />
+        <AddTodo addTodo={addTodo} />
         <TodoList />
       </div>
     </div>
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddTodo() {
-  return <h2>AddTodo</h2>;
+function AddTodo({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  function handleClick() {
+    if (value.length) {
+      addTodo(value);
+      setValue("");
+    }
+  }
+
+  function handleChange(e) {
+    const inputValue = e.target.value;
+    setValue(inputValue);
+  }
+
+  function handleKeyDown(e) {
+    if (e.code === "NumpadEnter" || e.code === "Enter") {
+      addTodo(value);
+      setValue("");
+    }
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Ajouter une todo"
+        className="me-4"
+        value={value} // pour faire de cet input un champ contrôlé (la valeur de cet input est toujours synchronisée avec l’état local du composant)
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handleClick}>Ajouter</button>
+    </div>
+  );
 }
 
 export default AddTodo;
